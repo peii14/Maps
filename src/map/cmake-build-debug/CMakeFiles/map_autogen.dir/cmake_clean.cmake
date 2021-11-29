075dbdaca054b94ebe0a26541633e201f80555fd
Gayuh Kautaman <61355731+peii14@users.noreply.github.com>
2021-11-29 20:43:41 +0100

    Delete src directory

--- src/map/cmake-build-debug/CMakeFiles/map_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/map_autogen"
-  "map_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/map_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- src/map/cmake-build-debug/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "map"
-PROJECT_SPACE_DIR = "/home/pei/Maps/src/map/cmake-build-debug/devel"
-PROJECT_VERSION = "0.0.0"
